a777d6b1e047f9f9101ba0958fc90542c7483848
semenov.v <semenov.v@websterstd.com>
2022-08-04 18:18:40 +0300

    добавлены блоки Ðо люÐÑ'о лÑлюдях и делиться своими фотоÐ, поправлена шапка

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ function scrollHeader() {
   const logo = document.querySelector(".header__logo svg");
   const button = document.querySelector(".header__button");
   const icon_phone = document.querySelector(".header__phone svg");
-  if (pageYOffset > 5) {
+  if (pageYOffset > 100) {
     header.classList.add("scrolled");
     logo.classList.add("scrolled");
     button.classList.add("scrolled");
