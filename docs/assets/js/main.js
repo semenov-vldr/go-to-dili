d6447e9b9d7bba32360f074905ba0752c398581b
Ruslan Madaminov <mruslan@websterstd.com>
2022-08-24 15:11:56 +0300

    обновления перед демоснтрацией Заказчику

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -363,47 +363,6 @@ if ( history_block ) {
 }
 
 
-if (document.documentElement.clientWidth < 768) {
-
-  new Swiper('.share__images', {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-
-    slidesPerView: 3,
-
-    // Откл функционала, если слайдов меньше, чем нужно
-    watchOverflow: true,
-
-    // Отступ между слайдами
-    spaceBetween: 15,
-
-    // Активный слайд по центру
-    initialSlides: false,
-    // Стартовый слайд
-    initialSlide: 0,
-
-    // Брейк поинты (адаптив)
-    // Ширина экрана
-    breakpoints: {
-      320: {
-        slidesPerView: 1.1
-      },
-      480: {
-        slidesPerView: 2.2
-      },
-      768: {
-        slidesPerView: 4
-      },
-    },
-
-  });
-
-
-}
-
-
 new Swiper('.places__list', {
   pagination: {
     el: '.swiper-pagination',
@@ -488,6 +447,51 @@ new Swiper('.places__list', {
 //
 //
 
+if (document.documentElement.clientWidth < 768) {
+
+  new Swiper('.share__images', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    slidesPerView: 3,
+
+    // Откл функционала, если слайдов меньше, чем нужно
+    watchOverflow: true,
+
+    // Отступ между слайдами
+    spaceBetween: 15,
+
+    // Активный слайд по центру
+    initialSlides: false,
+    // Стартовый слайд
+    initialSlide: 0,
+
+    // Брейк поинты (адаптив)
+    // Ширина экрана
+    breakpoints: {
+      320: {
+        slidesPerView: 1.1
+      },
+      480: {
+        slidesPerView: 2.2
+      },
+      768: {
+        slidesPerView: 4
+      },
+    },
+
+  });
+
+
+}
+
+
+
+
+
+
 new Swiper('.tours__list', {
   pagination: {
     el: '.swiper-pagination',
@@ -525,10 +529,6 @@ new Swiper('.tours__list', {
   }
 
 });
-
-
-
-
 
 const customJson = [
   // Eat
