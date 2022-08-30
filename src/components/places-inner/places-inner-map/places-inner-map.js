b0a59aabcdaa545ea377e9b10f82a3cd1e0b75c0
Vladimir <vova2066@yandex.ru>
2022-08-31 00:09:37 +0300

    ref func map

--- src/components/places-inner/places-inner-map/places-inner-map.js
+++ src/components/places-inner/places-inner-map/places-inner-map.js
@@ -1,32 +1,179 @@
 /* Map Yandex */
 
+// {
+
+//   const obj = [
+//     {
+//       type: "eat",
+//       mark_name: "mark-eat",
+//       photo_url: "./assets/img/map/01.jpg",
+//       title: "Dilijazz Restaurant 1",
+//       desc: 'Средиземноморская, Американска, Европейская',
+//       address: '1/1 2nd St., T’eghut, Дилижан 3902 Армения',
+//       location: [40.75826541732117, 44.88379503930662],
+//     },
+//     ]
 
 
+// // places-inner
+// const placesInnerMap = document.querySelector('.places-inner-map__wrapper');
+
+// let placesInnerMapItem;
+// let balloonTemplate;
+
+// if (placesInnerMap) {
+//   placesInnerMapItem = placesInnerMap.querySelector('.places-inner-map__item');
+// // Шаблон для клонирования
+//   balloonTemplate = placesInnerMap.querySelector('.balloon-template').content.querySelector('.balloon');
+// }
+
+// // Создание и заполнение данными баллуна
+//   const createBalloon = ( { photo_url, title, desc, address } ) => {
+//     const balloon = balloonTemplate.cloneNode(true);
+
+//     balloon.querySelector('.balloon__image').rsc = photo_url;
+//     balloon.querySelector('.balloon__title').textContent = title;
+//     balloon.querySelector('.balloon__desc-text').textContent = desc;
+//     balloon.querySelector('.balloon__address-text').textContent = address;
+
+//     return balloon.outerHTML;
+//   };
+
+// // Функция отрисовки меток на карте
+//   const renderMark = (navElems, arrObj) => {
+//     // Создание пустой коллекции геообъектов
+//     let geoObjects = new ymaps.GeoObjectCollection({});
+
+//     navElems.forEach(item => {
+//       if (item.classList.contains('js-active-mark')) {
+//         const dataType = item.dataset.type;
+
+//         arrObj.forEach(obj => {
+
+//           const balloonLayout = ymaps.templateLayoutFactory.createClass(createBalloon(obj), {
+
+//             build: function () {
+//               this.constructor.superclass.build.call(this);
+
+//               this._$element = $('.balloon', this.getParentElement());
+
+//               this._$element.find('.balloon__close')
+//                 .on('click', $.proxy(this.onCloseClick, this));
+//             },
+
+//             clear: function () {
+//               this._$element.find('.balloon__close').off('click');
+
+//               this.constructor.superclass.clear.call(this);
+//             },
+//             //
+//             // Закрывает балун при клике на крестик
+//             onCloseClick: function (evt) {
+//               evt.preventDefault();
+//               this.events.fire('userclose');
+//             },
+
+//           });
+
+//           // Баллун в виде панели при мобильном разрешении экрана
+//           if (window.matchMedia("(max-width: 580px)").matches) {
+//             map.options.set({balloonPanelMaxMapArea:'Infinity'}) ;
+//           }
+
+//           const {type, location, mark_name} = obj;
+
+//           if (type === dataType) {
+
+//             // Создание кастомных меток и баллунов
+//             const placemark = new ymaps.Placemark(location, {
+//               balloonContentBody: createBalloon(obj),
+//             }, {
+//               balloonLayout,
+//               hideIconOnBalloonOpen: false,
+//               iconLayout: 'default#image',
+//               iconImageHref: `./assets/img/map/${mark_name}.svg`,
+//               iconImageSize: [49, 59],
+//               iconImageOffset: [0, -60],
+//             });
+
+//             // Добавление метки в коллекцию
+//             geoObjects.add(placemark);
+//             // Добавление коллекции на карту
+//             map.geoObjects.add(geoObjects);
+//             // Установка масштаба для видимости всей коллекции
+//             map.setBounds(geoObjects.getBounds());
+//             // итоговый масштаб карты чуть меньше зоны видимости
+//             map.setZoom(map.getZoom() - 2);
+
+//           }
+//         });
+//       };
+//     });
+//   };
+
+// let map;
+
+// if ( placesInnerMapItem ) {
+
+//   ymaps.ready(init);
+
+//   // Инициализация карты
+//   function init() {
+//     map = new ymaps.Map("places-inner-map__item", {
+//         center: [40.74521099740435, 44.868688838134744],
+//         zoom: 13,
+//         controls: ['routeButtonControl'],
+//       },
+
+//       { //ограничения области просмотра карты
+//         restrictMapArea: [
+//           [39.874858480470486,42.27403199633786],
+//           [41.82876820666636,47.54746949633786]
+//         ]
+//       });
+
+//     let clusterer = new ymaps.Clusterer({});
+//     map.geoObjects.add(clusterer);
+
+//   };
+// }
+
+//   }
+
+/* Map Yandex */
 {
 
-  const obj = [
+
+  const Json = [
     {
-      type: "eat",
-      mark_name: "mark-eat",
+      mark_name: "mark-logo",
       photo_url: "./assets/img/map/01.jpg",
-      title: "Dilijazz Restaurant 1",
-      desc: 'Средиземноморская, Американска, Европейская',
+      title: "Техут",
+      desc: 'На перекрестке Дилижана и Иджевана',
       address: '1/1 2nd St., T’eghut, Дилижан 3902 Армения',
-      location: [40.75826541732117, 44.88379503930662],
+      location: [40.75326541732117, 44.88379503930662],
     },
-    ]
+
+    ];
 
 
-// places-inner
-const placesInnerMap = document.querySelector('.places-inner-map__wrapper');
+  // класс блока-обертки, внутри которого находится карта
+  let wrapperMapClass = '.places-inner-map__wrapper';
+  // класс блока с картой
+  let mapClass = '.places-inner-map__item';
+  // id карты
+  let mapId = 'places-inner-map__item';
 
-let placesInnerMapItem;
+
+const wrapper = document.querySelector(wrapperMapClass);
+
+let mapItem;
 let balloonTemplate;
 
-if (placesInnerMap) {
-  placesInnerMapItem = placesInnerMap.querySelector('.places-inner-map__item');
+if (wrapper) {
+  mapItem = wrapper.querySelector(mapClass);
 // Шаблон для клонирования
-  balloonTemplate = placesInnerMap.querySelector('.balloon-template').content.querySelector('.balloon');
+  balloonTemplate = document.querySelector('.balloon-template').content.querySelector('.balloon');
 }
 
 // Создание и заполнение данными баллуна
@@ -42,13 +189,10 @@ if (placesInnerMap) {
   };
 
 // Функция отрисовки меток на карте
-  const renderMark = (navElems, arrObj) => {
+  const renderMark = (arrObj) => {
     // Создание пустой коллекции геообъектов
     let geoObjects = new ymaps.GeoObjectCollection({});
 
-    navElems.forEach(item => {
-      if (item.classList.contains('js-active-mark')) {
-        const dataType = item.dataset.type;
 
         arrObj.forEach(obj => {
 
@@ -82,13 +226,11 @@ if (placesInnerMap) {
             map.options.set({balloonPanelMaxMapArea:'Infinity'}) ;
           }
 
-          const {type, location, mark_name} = obj;
-
-          if (type === dataType) {
+          const {location, mark_name} = obj;
 
             // Создание кастомных меток и баллунов
             const placemark = new ymaps.Placemark(location, {
-              balloonContentBody: createBalloon(obj),
+              balloonContentBody: createBalloon(Json),
             }, {
               balloonLayout,
               hideIconOnBalloonOpen: false,
@@ -103,25 +245,25 @@ if (placesInnerMap) {
             // Добавление коллекции на карту
             map.geoObjects.add(geoObjects);
             // Установка масштаба для видимости всей коллекции
-            map.setBounds(geoObjects.getBounds());
-            // итоговый масштаб карты чуть меньше зоны видимости
-            map.setZoom(map.getZoom() - 2);
+            //map.setBounds(geoObjects.getBounds());
 
-          }
+            // итоговый масштаб карты чуть меньше зоны видимости
+            //map.setZoom(map.getZoom() - 5);
+
+
         });
-      };
-    });
+
   };
 
 let map;
 
-if ( placesInnerMapItem ) {
+if ( mapItem ) {
 
   ymaps.ready(init);
 
   // Инициализация карты
   function init() {
-    map = new ymaps.Map("places-inner-map__item", {
+    map = new ymaps.Map(mapId, {
         center: [40.74521099740435, 44.868688838134744],
         zoom: 13,
         controls: ['routeButtonControl'],
@@ -134,10 +276,12 @@ if ( placesInnerMapItem ) {
         ]
       });
 
+      renderMark(Json);
+
     let clusterer = new ymaps.Clusterer({});
     map.geoObjects.add(clusterer);
 
   };
 }
 
-  }
+}
