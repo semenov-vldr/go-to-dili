9ae7ec03edd2144510e8d429658b3de31cd91232
Vladimir <vova2066@yandex.ru>
2022-10-25 15:05:12 +0300

    ref ci.yml

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -597,47 +597,6 @@ new Swiper('.places__list', {
 
 }
 
-new Swiper('.tours__list', {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  slidesPerView: 4,
-
-  // Откл функционала, если слайдов меньше, чем нужно
-  watchOverflow: true,
-
-  centeredSlides: true,
-
-  // Отступ между слайдами
-  spaceBetween: 15,
-
-  // Стартовый слайд
-  initialSlide: 0,
-
-  loop: true,
-
-  // Брейк поинты (адаптив)
-  // Ширина экрана
-  breakpoints: {
-    320: {
-      slidesPerView: 1.2,
-    },
-    480: {
-      slidesPerView: 2.2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1400: {
-      slidesPerView: 4,
-      centeredSlides: false,
-    },
-  }
-
-});
-
 /* Map Yandex */
 
 {
@@ -706,6 +665,47 @@ new Swiper('.tours__list', {
   });
 
 }
+
+new Swiper('.tours__list', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  slidesPerView: 4,
+
+  // Откл функционала, если слайдов меньше, чем нужно
+  watchOverflow: true,
+
+  centeredSlides: true,
+
+  // Отступ между слайдами
+  spaceBetween: 15,
+
+  // Стартовый слайд
+  initialSlide: 0,
+
+  loop: true,
+
+  // Брейк поинты (адаптив)
+  // Ширина экрана
+  breakpoints: {
+    320: {
+      slidesPerView: 1.2,
+    },
+    480: {
+      slidesPerView: 2.2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 4,
+      centeredSlides: false,
+    },
+  }
+
+});
 
 
 
