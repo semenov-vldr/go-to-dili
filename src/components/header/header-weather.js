a9e2d8e6d537c6f9e6fc4be60a42af481a20b37f
Vladimir <vova2066@yandex.ru>
2022-10-27 10:50:04 +0300

    add api weather

--- src/components/header/header-weather.js
+++ src/components/header/header-weather.js
@@ -1,24 +1,41 @@
 const header = document.querySelector('.header');
-
 const weatherTemp = header.querySelector('.header__weather-temp');
 
-
-const API_KEY = 'fd04c038b4083dd0d159274298038549';
+const KEY = '8c0d97481931b1c092950c47648f9df4';
 const city = 'Dilijan';
-const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+const API = `http://api.weatherstack.com/current?access_key=${KEY}&query=${city}`;
 
-
-
-fetch(API)
-  .then(responce => responce.json())
-  .then(data => {
-    console.log(data.main.temp);
-      let temp = `${Math.round(data.main.temp - 273)}°C`;
-      weatherTemp.textContent = temp;
-  })
-  .catch(err => {
+const fetchData = async () => {
+  try{
+    const result = await fetch(API);
+    const data = await result.json();
+    const temp = `${data.current.temperature}°C`;
+    weatherTemp.textContent = temp;
+  } catch (err) {
     console.log(err);
-    console.log('Ошибка доступа к погоде')
-    weatherTemp.textContent = '+10°C';
-  })
+  }
+
+}
+
+fetchData()
+
+
+
+
+// const API_KEY = 'fd04c038b4083dd0d159274298038549';
+// const city = 'Dilijan';
+// const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+//
+// fetch(API)
+//   .then(responce => responce.json())
+//   .then(data => {
+//     console.log(data.main.temp);
+//       let temp = `${Math.round(data.main.temp - 273)}°C`;
+//       weatherTemp.textContent = temp;
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     console.log('Ошибка доступа к погоде')
+//     weatherTemp.textContent = '+10°C';
+//   })
 
